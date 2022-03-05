5b95b4c5cf72ebc5ddf7a970d15bf4b5ff8f1a74
renmurakami <autoeyy@gmail.com>
2022-03-05 14:22:24 +0900

    reactでデータ取れた

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,41 @@
-import { Box, Text } from "@chakra-ui/react";
+import { gql, useQuery } from "@apollo/client";
+import { Box, Button, Text } from "@chakra-ui/react";
 import { useState, VFC } from "react";
 
 const App: VFC = () => {
+	const [posts, setPost] = useState<[]>([]);
+	const postGql = gql`
+		query {
+			posts {
+				id
+				title
+				content
+				isPublished
+			}
+		}
+	`;
+
+	const { loading, error, data } = useQuery(postGql);
+	const graphqlQueryTest = () => {
+		console.log(data.posts);
+		setPost(data.posts);
+	};
+
+	if (loading) return <Text>loading...</Text>;
+
+	if (error) return <Text>err</Text>;
+
 	return (
 		<Box>
 			<Text>hello react nest prisma graphql</Text>
+			<Button onClick={graphqlQueryTest}>gql Query!</Button>
+			{posts.map((d: any) => (
+				<Box key={d.id}>
+					<Text>{d.id}</Text>
+					<Text>{d.title}</Text>
+					<Text>{d.content}</Text>
+				</Box>
+			))}
 		</Box>
 	);
 };
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,13 +1,21 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+const client = new ApolloClient({
+	uri: "http://localhost:3001/graphql",
+	cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
 	<React.StrictMode>
-		<ChakraProvider>
-			<App />
-		</ChakraProvider>
+		<ApolloProvider client={client}>
+			<ChakraProvider>
+				<App />
+			</ChakraProvider>
+		</ApolloProvider>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
